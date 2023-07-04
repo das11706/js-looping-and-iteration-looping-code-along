79cc5f97d5273a4a4adc173806b176eb0a8a6462
Daniel Suarez <suarez.daniel101@gmail.com>
2023-07-04 15:52:24 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+
+function writeCards(names, event) {
+  let thanksArray = []
+
+  for(let i = 0; i < names.length; i++) {
+    thanksArray.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+  }
+
+  return thanksArray;
+
+}
+
+function countDown(count) {
+  while(count > 0) {
+    console.log(count);
+    count--;
+  }
+  console.log(count);
+}
+
